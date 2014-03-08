e9767703b712ca04c6d1b0d71d5333f6e45b1375
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-03-08 21:57:19 +0200

    Added initial files

---
+++ src/ref.js
@@ -0,0 +1,38 @@
+/* Hypermedia References */
+
+"use strict";
+
+var path = require('path');
+
+/** Check if connection is HTTPS */
+function is_https(req) {
+	if(req.headers && req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === 'https') { return true; }
+	return (req && req.connection && req.connection.encrypted) ? true : false;
+}
+
+/** Get protocol part of the url */
+function get_proto(req) {
+	return is_https(req) ? 'https' : 'http';
+}
+
+/** Get hostname part of the url */
+function get_host(req) {
+	return req.headers && req.headers.host;
+}
+
+// FIXME: Escape args
+function format_arg(a) {
+	return ''+a;
+}
+
+/** Get ref */
+function ref() {
+	var args = Array.prototype.slice.call(arguments);
+	var req = args.shift();
+	return get_proto(req) + ':/'+'/' + get_host(req) + path.resolve.apply(path.resolve, ['/'].concat(args).map(format_arg));
+}
+
+// Exports
+module.exports = ref;
+
+/* EOF */
