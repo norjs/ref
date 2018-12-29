a876e7a999eb35fab4f1adf029cd903bd54f1144
Jaakko-Heikki Heusala <jhh@sendanor.com>
2018-12-30 01:10:57 +0200

    Converted as ES6, babel setup, and other packaging

--- src/ref.js
+++ src/ref.js
@@ -1,45 +1,65 @@
 /* Hypermedia References */
 
-"use strict";
+import _ from 'lodash';
+import path from 'path';
 
-var path = require('path');
-var ARRAY = require('nor-array');
-var FUNCTION = require('nor-function');
-
-/** Check if connection is HTTPS */
-function is_https(req) {
-	if(req.headers && req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === 'https') { return true; }
-	return (req && req.connection && req.connection.encrypted) ? true : false;
+/** Check if connection is HTTPS
+ * @param req
+ * @return {boolean}
+ */
+function is_https (req) {
+	if (req.headers && req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === 'https') {
+		return true;
+	}
+	return !!(req && req.connection && req.connection.encrypted);
 }
 
-/** Get protocol part of the url */
-function get_proto(req) {
+/** Get protocol part of the url
+ * @param req
+ * @return {string}
+ */
+function get_proto (req) {
 	return is_https(req) ? 'https' : 'http';
 }
 
-/** Get hostname part of the url */
-function get_host(req) {
-	return req.headers && req.headers.host;
+/** Get hostname part of the url
+ * @param req
+ * @return {*}
+ */
+function get_host (req) {
+	return req.headers ? req.headers.host : undefined;
 }
 
-// FIXME: Escape args
-function format_arg(a) {
+/**
+ *
+ * @FIXME: Escape args
+ * @param a
+ * @returns {string}
+ */
+function format_arg (a) {
 	return ''+a;
 }
 
-/** Get ref */
-function ref(base_path) {
-	var args2 = ['/'].concat(Array.prototype.slice.call(arguments));
-	return function _ref() {
-		var args = Array.prototype.slice.call(arguments);
-		var req = args.shift();
-		args = args2.concat(args);
-		return get_proto(req) + ':/'+'/' + get_host(req) + FUNCTION(path.resolve).apply(path.resolve, ARRAY([base_path]).concat(args).map(format_arg).valueOf() );
+/** Get ref
+ *
+ * @param args
+ * @return {function(...[*]=): string}
+ */
+function ref (...args) {
+	const base_path = _.first(args);
+	const args2 = _.concat(['/'], args);
+	return ( ...args ) => {
+		const req = args.shift();
+		args = _.concat(args2, args);
+		const args3 = _.concat([base_path], args).map(format_arg);
+		return `${get_proto(req)}://${get_host(req)}${path.resolve(...args3)}`;
 	};
-};
+}
 
 // Exports
-var _root_ref = module.exports = ref('/');
-_root_ref.withPath = ref;
+let rootRef = ref('/');
+rootRef.withPath = ref;
+
+export default rootRef;
 
 /* EOF */
